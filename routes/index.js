7fbb2d8ec64f813a3f43cd087c9d33272419ef5a
nosql <nosql@wu-etu-03929.univ-lyon1.fr>
2019-05-16 11:29:12 +0200

    Ajout du select *

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var posts = require("./posts");
 
+var resultArray = [];
+
 module.exports = function(app,client){
 
   app.get("/", function(req, res) {
-    res.render("index");
+
+    app.client = client;
+    app.db = client.db("Blog");
+
+    var cursor = app.db.collection("Articles").find();
+    console.log(app.db.collection("Articles").find());
+    cursor.forEach(function(doc, err) {
+      resultArray.push(doc);
+    },function() {
+      client.close();
+      app.render('index', {'items': resultArray});
+    });
   });
 
   // Register posts endpoint
